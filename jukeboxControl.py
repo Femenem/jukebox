457f821c19dd4254f25aca8fbfbfe6fe5faa6329
Matt <dozy-morgan@hotmail.co.uk>
2017-09-04 14:26:38 +0100

    debugging

--- jukeboxControl.py
+++ jukeboxControl.py
@@ -2,12 +2,11 @@ import time
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 import os
-import threading
 
 from neopixel import *
 
 # LED strip configuration:
-LED_COUNT      = 144      # Number of LED pixels.
+LED_COUNT      = 144     # Number of LED pixels.
 LED_PIN        = 13      # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
@@ -16,36 +15,37 @@ LED_INVERT     = False   # True to invert the signal (when using NPN transistor 
 LED_CHANNEL    = 1       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
-CLK  = 17
-MISO = 27
-MOSI = 6
-CS   = 26
-adc = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+# ADC configuration:
+CLK  = 17	# Clock pin
+MISO = 27	# MISO pin
+MOSI = 6	# MOSI pin
+CS   = 26	# CS pin
+adc = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)	# Intialize the ADC
 
+# Global music settings
 volume = 50
 bass = 50
 treble = 50
 
-class MainControl(threading.Thread):
-	global ledMain
+class MainControl():
+	global ledMain, adc
 	def __init__(self, volume, bass, treble):
-		threading.Thread.__init__(self)
 		self.volume = volume
 		self.bass = bass
 		self.treble = treble
 
 	def run(self):
 		print("Main thread begun")
+		leds = LedControl(self.volume, self.bass, self.treble)
 		while True:
-			while True:
-				print("Its looping!")
-				newVolume = self.read_volume()
-				if (newVolume < self.volume - 2) or (newVolume > self.volume + 2):
-					#The volume knob has been changed so we change the volume through alsa.
-					os.system("amixer set Master "+str(newVolume)+"%")
-					self.volume = newVolume
-					ledMain.volume_led(self.volume)
-					print("Volume set to "+str(self.volume))
+			print("Its looping!")
+			newVolume = self.read_volume()
+			if (newVolume < self.volume - 2) or (newVolume > self.volume + 2):
+				#The volume knob has been changed so we change the volume through alsa.
+				os.system("amixer set Master "+str(newVolume)+"%")
+				self.volume = newVolume
+				leds.volume_led(self.volume)
+				print("Volume set to "+str(self.volume))
 
 			#print("Volume: "+ str(read_volume()))
 			#print("Bass: "+ str(read_bass()))
@@ -66,7 +66,7 @@ class MainControl(threading.Thread):
 		number = round(number/10.24)
 		return int(number)
 
-class LedControl(threading.Thread):
+class LedControl():
 	red = Color(255, 0, 0)
 	green = Color(0, 255, 0)
 	blue = Color(0, 0, 255)
@@ -74,16 +74,12 @@ class LedControl(threading.Thread):
 	global volume, bass, treble
 
 	def __init__(self, volumeNumber, bassNumber, trebleNumber):
-		threading.Thread.__init__(self)
 		self.volumeNumber = round(volume*34/100)
 		self.bassNumber = round(bass*34/100)
 		self.trebleNumber = round(treble*34/100)
 		# Create NeoPixel object with appropriate configuration.
 		self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
  	   	# Intialize the library (must be called once before other functions).
-
-	def run(self):
-		print("Led thread begun")
 		self.strip.begin()
 
 	def volume_led(self, percent):
@@ -93,17 +89,17 @@ class LedControl(threading.Thread):
 		if newVolume < self.volumeNumber:
 			#paint right pixels black
 			i = 61
-			while i < i + newVolume:
+			while i < 61 + newVolume:
 				self.strip.setPixelColor(i, self.black)
 				i += 1
 			self.strip.show()
 			print("changing leds for volume")
 		else:
 			#paint right pixels colour
-			i = 61
-			while i < i + newVolume:
+			i = 95
+			while i > 95 - newVolume:
 				self.strip.setPixelColor(i, self.red)
-				i += 1
+				i -= 1
 			self.strip.show()
 			print("changing leds for volume")
 
@@ -125,8 +121,6 @@ class LedControl(threading.Thread):
 
 print("got to here")
 main = MainControl(volume, bass, treble)
-ledMain = LedControl(volume, bass, treble)
 print("got to here")
-main.start()
-ledMain.start()
+main.run()
 print("got to here")
