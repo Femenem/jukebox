6300db94310070cef636ec901d84b1d500c9a9b5
Matt <dozy-morgan@hotmail.co.uk>
2017-09-04 16:42:21 +0100

    debugging

--- jukeboxControl.py
+++ jukeboxControl.py
@@ -125,10 +125,21 @@ class LedControl():
 			i += 1
 		self.strip.show()
 
+	def wheel(self, pos):
+		if pos < 85:
+			return Color(pos*3,255-pos*3,0)
+		elif pos < 170:
+			pos -= 85
+			return Color(255-pos*3,0,pos*3)
+		else:
+			pos -= 170
+			return Color(0,pos*3,255-pos*3)
+
 	def playing_leds(self):
-		for i in range(self.strip.numPixels()):
-			self.strip.setPixelColor(i, Color(self.random_255(), self.random_255(), self.random_255()))
-		self.strip.show()
+		for j in range(256*5):
+			for i in range(self.strip.numPixels()):
+				self.strip.setPixelColor(i, wheel((int(i*256/strip.numPixels())+j) & 255))
+			self.strip.show()
 
 	def random_255(self):
 		return random.randint(0, 255)
